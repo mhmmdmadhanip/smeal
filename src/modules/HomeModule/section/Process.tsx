de141adafbe358fe0b24e106c816da928aa39de2
Muhammad Madhani Putra <muhammad.madhani@ui.ac.id>
2025-03-04 15:40:41 +0700

    feat: Process Mobile

--- src/modules/HomeModule/section/Process.tsx
+++ src/modules/HomeModule/section/Process.tsx
@@ -14,33 +14,33 @@ const Process = () => {
         <Image 
             src={bottomLine}
             alt='Random Line'
-            className='w-full object-cover absolute top-0 left-0'
+            className='w-full object-cover absolute top-[10%] lg:top-0 left-0'
         />
-        <p className='text-white text-[56px] font-bold leading-[120%]'>
+        <p className='text-white text-[32px] md:text-[56px] font-bold leading-[120%]'>
             Our Process
         </p>
-        <div className='grid 2xl:grid-cols-6 lg:grid-cols-3 grid-cols-1 gap-6 relative text-white font-bold text-2xl leading-[120%] pb-20'>
-            <div className='px-7 py-5 bg-[#259426] absolute bottom-10 left-0'>
+        <div className='grid 2xl:grid-cols-6 lg:grid-cols-3 grid-cols-1 self-center gap-6 relative text-white font-bold sm:text-2xl leading-[120%] pb-20'>
+            <div className='px-4 py-3 sm:px-7 sm:py-5 bg-[#259426] absolute 2xl:bottom-10 left-[-3rem] sm:left-[-9rem] 2xl:left-0'>
                 <p>
                     Easy Packaging
                 </p>
             </div>
-            <div className='px-7 py-5 bg-[#34B335] absolute bottom-0 left-[16rem]'>
+            <div className='px-4 py-3 sm:px-7 sm:py-5 bg-[#34B335] absolute top-[30%] lg:bottom-[50%] 2xl:bottom-0 left-[-3rem] sm:left-[-8rem] 2xl:left-[16rem]'>
                 <p>
                 Scheduled Pick-Up
                 </p>
             </div>
-            <div className='px-7 py-5 bg-[#259426] absolute bottom-10 left-[35rem]'>
+            <div className='px-4 py-3 sm:px-7 sm:py-5 bg-[#259426] absolute top-[15%] lg:top-0 2xl:bottom-10 right-[-3rem] sm:right-[-10rem] lg:left-[35rem] w-max'>
                 <p>
                 Flexible Payment
                 </p>
             </div>
-            <div className='px-7 py-5 bg-[#34B335] absolute bottom-0 right-[23rem]'>
+            <div className='px-4 py-3 sm:px-7 sm:py-5 bg-[#34B335] absolute h-fit top-[50%] lg:bottom-8 2xl:bottom-0 right-[-3rem] sm:right-[-7rem] 2xl:right-[23rem]'>
                 <p>
                 Pick-Up Station
                 </p>
             </div>
-            <div className='px-7 py-5 bg-[#259426] absolute bottom-10 right-10'>
+            <div className='px-4 py-3 sm:px-7 sm:py-5 bg-[#259426] absolute bottom-[20%] lg:bottom-10 left-[-3rem] sm:left-[-10rem] 2xl:right-10'>
                 <p>
                 Transparent Nutritiont
                 </p>
